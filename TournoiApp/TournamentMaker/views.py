3402b7c0fc8beb65cb9b6c9d1de3dd62c960dd35
= <=>
2025-05-16 00:16:52 +0200

    add lien entre classe plus detail

--- TournoiApp/TournamentMaker/views.py
+++ TournoiApp/TournamentMaker/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from TournamentMaker.models import Player
+from TournamentMaker.models import Player, Team
 
 def index(request):
     """View function for home page of site."""
@@ -17,3 +17,30 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+from django.shortcuts import render
+
+def players(request):
+    all_players = Player.objects.all()  # récupère tous les joueurs
+    return render(request, 'players.html', {'players': all_players})
+
+def teams(request):
+    all_teams = Team.objects.all()
+
+    return render(request, 'teams.html', {'teams': all_teams})
+
+def scores(request):
+    
+    return render(request, 'scores.html')
+
+
+from django.shortcuts import render, get_object_or_404
+from TournamentMaker.models import Player, Team
+
+def player_detail(request, pk):
+    player = get_object_or_404(Player, pk=pk)
+    return render(request, 'players_detail.html', {'player': player})
+
+def team_detail(request, pk):
+    team = get_object_or_404(Team, pk=pk)
+    return render(request, 'teams_detail.html', {'team': team})
