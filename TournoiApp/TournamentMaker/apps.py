08d8ca18424323ddd28ee02984c4571d5e8eb3e7
huangantoine <antoine.huang@edu.esiee.fr>
2025-05-20 08:36:09 +0200

    Sauvegarde temporaire avant rebase

--- TournoiApp/TournamentMaker/apps.py
+++ TournoiApp/TournamentMaker/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class TournamentmakerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'TournamentMaker'
-
-def ready(self):
-    import TournamentMaker.signals  # adapte si ton app a un autre nom
