8d5a99c0a006e626252282a84cf446631982ecd1
pablo-minelian <pablo.minelian@edu.esiee.fr>
2025-05-20 16:51:29 +0200

    Bouton Tournoi

--- TournoiApp/TournamentMaker/views.py
+++ TournoiApp/TournamentMaker/views.py
@@ -124,3 +124,22 @@ def scores(request):
     matches = Match.objects.all()
     return render(request, "scores.html", {"matches": matches})
 
+from .models import Tournament
+
+def select_tournament(request):
+    if request.method == 'POST':
+        selected_id = request.POST.get('tournament_id')
+        if selected_id:
+            request.session['selected_tournament_id'] = selected_id
+            return redirect('index')
+
+    tournois = Tournament.objects.all()
+    return render(request, 'select_tournament.html', {'tournois': tournois})
+
+def player_list(request):
+    tournament_id = request.session.get('selected_tournament_id')
+    if not tournament_id:
+        return redirect('select_tournament')
+
+    players = Player.objects.filter(team__tournament_id=tournament_id)
+    return render(request, 'players.html', {'players': players})
