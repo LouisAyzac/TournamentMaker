333674b5068c7814c3df2a5dfc35cbae9b8c4ac3
= <=>
2025-05-26 00:38:03 +0200

    merge ange

--- TournoiApp/TournamentMaker/models.py
+++ TournoiApp/TournamentMaker/models.py
@@ -2,11 +2,9 @@ import random
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-<<<<<<< HEAD
 from django.contrib.auth.models import User
-=======
 from datetime import date
->>>>>>> ange
+
 
 
 class Tournament(models.Model):
