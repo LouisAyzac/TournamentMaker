292ab28c59b177ee1dbf1f4053eac23e1147af06
= <=>
2025-05-27 20:46:15 +0200

    merge remy

--- TournoiApp/TournamentMaker/urls.py
+++ TournoiApp/TournamentMaker/urls.py
@@ -22,13 +22,13 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signup/success/', views.signup_success, name='signup_success'),
     path('matchs-en-cours/', views.matchs_en_cours, name='matchs_en_cours'),
-<<<<<<< HEAD
+    
     path('classement/final/', views.classement_final_view, name='classement_final'),
     path('classement/final/', views.classement_final_view, name='rankings'),
-=======
+
     path('matchs/', views.matchs, name='matchs'),
 
->>>>>>> Remy
+
     
 ]
 
