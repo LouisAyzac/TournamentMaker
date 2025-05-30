dc4d92b918735571ddb5920ba967faac21822a90
huangantoine <antoine.huang@edu.esiee.fr>
2025-05-30 17:46:02 +0200

    correction erreur inscription

--- TournoiApp/TournamentMaker/models.py
+++ TournoiApp/TournamentMaker/models.py
@@ -242,7 +242,7 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        pass
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
