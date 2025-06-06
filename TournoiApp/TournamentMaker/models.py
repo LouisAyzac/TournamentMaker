024313c183c8f9a79b96ae6940386ce73b9498cf
LouisAyzac <louis.ayzac@edu.esiee>
2025-06-06 12:19:18 +0200

    a

--- TournoiApp/TournamentMaker/models.py
+++ TournoiApp/TournamentMaker/models.py
@@ -242,11 +242,9 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-<<<<<<< HEAD
-        UserProfile.objects.create(user=instance)
-=======
+
         pass
->>>>>>> antoine
+    
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
