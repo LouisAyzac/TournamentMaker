6526645bffff087b0130660d09b71cd997bb047d
pablo-minelian <pablo.minelian@edu.esiee.fr>
2025-06-13 10:18:33 +0200

    vendredi 13/06

--- TournoiApp/TournamentMaker/context_processors.py
+++ TournoiApp/TournamentMaker/context_processors.py
@@ -1,13 +1,17 @@
+from datetime import date
 from .models import Tournament
 
-def selected_tournament(request):
-    selected_id = request.session.get('selected_tournament_id')
-    tournoi = None
-    if selected_id:
-        try:
-            tournoi = Tournament.objects.get(id=selected_id)
-        except Tournament.DoesNotExist:
-            pass
-    return {
-        'tournoi': tournoi
-    }
+def hide_inscription_button(request):
+    try:
+        tournament_id = request.session.get('selected_tournament_id')
+        if not tournament_id:
+            return {'hide_inscription': False}
+        
+        tournoi = Tournament.objects.get(id=tournament_id)
+
+        is_full = tournoi.teams.count() >= tournoi.max_teams
+        is_over = tournoi.end_date < date.today()
+
+        return {'hide_inscription': is_full or is_over}
+    except:
+        return {'hide_inscription': False}
